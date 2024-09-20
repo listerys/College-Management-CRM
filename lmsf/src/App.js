413ca6578fa6f92ae1907c42a8fc68a75d601ff7
listerys <joysup111@yahoo.com>
2024-09-19 22:07:25 -0400

    Defined Routes

--- lmsf/src/App.js
+++ lmsf/src/App.js
@@ -1,5 +1,12 @@
 // App.js
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './AuthContext';
+import Login from './components/Login';
+import PrivateRoute from './components/PrivateRoute';
+import AdminDashboard from './components/AdminDashboard';
+import ProtectedComponent from './components/ProtectedComponent';
+import StudentDashboard from './components/StudentDashboard'; // Assume you have a StudentPage component
 import './App.css';
 
 function App() {
@@ -8,6 +15,8 @@ function App() {
       <Router>
         <Routes>
           <Route path="/login" element={<Login />} />
+          <Route path="/admin" element={<PrivateRoute component={AdminDashboard} requiredRole="admin" />} />
+          <Route path="/protected" element={<PrivateRoute component={ProtectedComponent} />} />
           <Route path="/" element={<PrivateRoute component={StudentDashboard} requiredRole="student" />} />
         </Routes>
       </Router>
