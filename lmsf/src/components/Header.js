50b9d76f300a68937ec83fec2356f9772d753fc9
listerys <joysup111@yahoo.com>
2024-09-21 09:28:42 -0400

    State management for dropdown

--- lmsf/src/components/Header.js
+++ lmsf/src/components/Header.js
@@ -1,6 +1,49 @@
 import React, { useState, useEffect } from "react";
 import "./Header.css"; // Ensure the CSS path is correct
 
+const Header = ({ onSelectTerm }) => {
+  const [selectedTerm, setSelectedTerm] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
+  const [yearOptions, setYearOptions] = useState([]);
+
+  useEffect(() => {
+    // Fetch academic years from the backend
+    const fetchYearOptions = async () => {
+      try {
+        const response = await fetch("http://localhost:3001/academicyears");
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        console.log("Fetched year options:", data);
+        setYearOptions(data.map((item) => item.yearname));
+      } catch (error) {
+        console.error("Error fetching academic years:", error);
+      }
+    };
+
+    fetchYearOptions();
+  }, []);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (event.target.closest(".dropdown") === null) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("click", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
+
+  const handleTermSelect = (option) => {
+    setSelectedTerm(option);
+    setIsOpen(false);
+    onSelectTerm(option);
+  };
 
   return (
     <div className="headerContainer">
